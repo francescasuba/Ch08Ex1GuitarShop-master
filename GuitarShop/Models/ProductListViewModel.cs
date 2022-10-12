1956d1f15b317cdc8e7ffb51c033f664feaf697f
subaap <subaap@VDI-CECH-IC-11>
2022-10-12 12:38:47 -0400

    final commit

---
+++ GuitarShop/Models/ProductListViewModel.cs
@@ -0,0 +1,24 @@
+﻿// FRANCESCA SUBA
+using System.Collections.Generic;
+
+namespace GuitarShop.Models
+{
+    public class ProductListViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+        public string SelectedCategory { get; set; }
+        public string CheckActiveCategory(string category) =>
+            category == SelectedCategory ? "active" : "";
+
+        public ProductListViewModel(List<Category> Categories, List<Product> Products, string SelectedCategory)
+        {
+            this.Categories = Categories;
+            this.Products = Products;
+            this.SelectedCategory = SelectedCategory;
+        }
+
+        
+
+    }
+}
